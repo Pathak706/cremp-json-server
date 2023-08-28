e66ed43b87478065b27232781d4522cfb1faad40
rahulpathak <rahul@carrot.club>
2023-08-28 20:00:47 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+
+server.post('/login', (req, res) => {
+  const db = router.db; // lowdb instance
+  const users = db.get('users').value();
+  const { username, password } = req.body;
+
+  const user = users.find(user => user.email === username && user.password === password);
+
+  if (user) {
+    res.json({ status: 'success', user: user });
+  } else {
+    res.status(401).json({ status: 'failure', message: 'Invalid username or password' });
+  }
+});
+
+server.post('/vendor/sign-up', (req, res) => {
+  const db = router.db;
+  const newUser = { ...req.body, role: 'vendor', email: req.body.email, password: req.body.userName };
+  db.get('users').push(newUser).write();
+  res.json({ status: 'success', message: 'Vendor successfully registered' });
+});
+
+server.post('/buyer/signup', (req, res) => {
+  const db = router.db;
+  const newUser = { ...req.body, role: 'buyer', email: req.body.email, password: req.body.userName };
+  db.get('users').push(newUser).write();
+  res.json({ status: 'success', message: 'Buyer successfully registered' });
+});
+
+server.use(router);
+server.listen(5000, () => {
+  console.log('JSON Server is running on http://localhost:5000/');
+});
